d108f8eca572d87b21b16872351e915052a6b560
clint <clint.fleetwood@gmail.com>
2020-02-09 14:14:13 -0500

    updated something

--- src/core.js
+++ src/core.js
@@ -8,3 +8,11 @@ export const pageManager = p;
 export const store = s;
 export const loadComponents = lc;
 export const createComponent = cc;
+
+/*
+export const uuid = () => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+*/
